0e5143fd47e68bc09644d02230ae42e01c317950
Itsaadarsh <aadarsh-s@outlook.com>
2021-04-03 21:18:08 +0530

    Insertion and Selection sort

--- sortAlgs/SelectionSort.java
+++ sortAlgs/SelectionSort.java
@@ -1,16 +1,16 @@
 package sortAlgs;
 
-/** BubbleSort */
-
-public class BubbleSort {
+public class SelectionSort {
     public static void main(String[] args) {
         int[] list = { -12, 56, 2, -542, 53, 44, 457, 4, -23, 0, 12, 234, 234, 2354, 36, -457, -312, -345, 23 };
         for (int i = 0; i < list.length; i++) {
-            for (int j = 1; j < (list.length - i); j++) {
-                if (list[j - 1] > list[j]) {
-                    swap(list, j - 1, j);
+            int largestNumIndex = 0;
+            for (int j = 1; j < list.length - i; j++) {
+                if (list[j] > list[largestNumIndex]) {
+                    largestNumIndex = j;
                 }
             }
+            swap(list, largestNumIndex, list.length - 1 - i);
         }
         display(list);
     }
