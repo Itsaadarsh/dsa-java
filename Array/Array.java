47fdeb9815c43fd912b17d8d33631e418e966dc8
Itsaadarsh <heyitsaadarsh@gmail.com>
2021-08-09 17:37:44 +0530

    Extending Array Methods

--- Array/Array.java
+++ Array/Array.java
@@ -44,4 +44,41 @@ public class Array {
         }
         return -1;
     }
+
+    public int max() {
+        int maxNum = -1;
+        for (int i = 0; i < count; i++) {
+            if (items[i] > maxNum) {
+                maxNum = items[i];
+            }
+        }
+        return maxNum;
+    }
+
+    public void reverse() {
+        int[] newItems = new int[count];
+        for (int i = 0; i < count; i++) {
+            newItems[i] = items[count - i - 1];
+        }
+        items = newItems;
+    }
+
+    public void insertAt(int index, int item) {
+        if (index < 0 || index >= count) {
+            throw new IllegalArgumentException();
+        }
+        if (count == items.length) {
+            int[] newItems = new int[count * 2];
+            for (int i = 0; i < count; i++) {
+                newItems[i] = items[i];
+            }
+            items = newItems;
+        }
+        for (int i = count - 1; i >= index; i--) {
+            items[i + 1] = items[i];
+        }
+
+        items[index] = item;
+        count++;
+    }
 }
--- Array/Main.java
+++ Array/Main.java
@@ -3,9 +3,12 @@ package Array;
 public class Main {
     public static void main(String[] args) {
         Array numbers = new Array(1);
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             numbers.insert(i + 1);
         }
-        System.out.println(numbers.indexOf(10));
+        // System.out.println(numbers.max());
+        // numbers.reverse();
+        numbers.insertAt(3, 0);
+        numbers.print();
     }
 }
